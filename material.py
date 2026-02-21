fb631e53b914091fd2da4b15819d7f4f07d171f3
Eduardo López <edolopez13@gmail.com>
2026-02-21 18:34:25 -0500

    Update material.py

--- material.py
+++ material.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 17 14:33:56 2025
-
+material module: Python script that imports data from an excel file (Data hub) which containing the material data extracted from the material database
 @author: ELC
 """
 
@@ -12,9 +11,18 @@ materialData = 'C:\\\\Users\\\\ELC\\\\Documents\\\\ModulesEL\\\\englib\\\\Materi
 
 
 class material():
+
     
     def __init__(self, material="Al2014 T6",sheetName = "METAL SI"):
-        
+       """
+       -Description:
+       Initiates a material object with it's properties for a specified material
+       -Input Parameters:
+       material Type: String
+       sheetName Type String
+       -Output:
+       Material object with data extracted from the excel file (Data Hub)
+       """
        materialDF = pd.read_excel(materialData,index_col="Material",sheet_name=sheetName)
        self.name = material
        self.youngModulus = materialDF.loc[material,"Young Modulus"]
@@ -27,5 +35,6 @@ class material():
     
     
     
+
 
         
